63622883311ce29e47bd65ca6e5570f9f0533883
Thailan <thailan-higor@hotmail.com>
2020-05-17 17:12:31 -0300

    Aplicação template da Home

--- app/controllers/home/views.py
+++ app/controllers/home/views.py
@@ -5,3 +5,8 @@ from app import db
 @home.route("/", methods=["GET"])
 def index():
     return render_template("home.html")
+
+
+@home.route("/sobre-mim", methods=["GET"])
+def sobre_mim():
+    return render_template("sobremim.html")
